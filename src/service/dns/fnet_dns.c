c39ab3a37134ed27c17e7ae9fc594d5c82019243
RoSchmi <Dr-Roland-Schmidt@t-online.de>
2021-03-31 14:33:15 +0200

    Added message FNET_TRUE

--- src/service/dns/fnet_dns.c
+++ src/service/dns/fnet_dns.c
@@ -21,6 +21,8 @@
 *  DNS Client/Resolver implementation.
 *
 ***************************************************************************/
+
+
 #include "fnet.h"
 
 #if FNET_CFG_DNS
@@ -597,6 +599,8 @@ static fnet_bool_t _fnet_dns_cmp_name(const char *rr_name, const char *name)
     fnet_uint32_t       name_length = fnet_strlen(name);
     fnet_uint32_t       rr_name_length = fnet_strlen(rr_name);
 
+    Serial.pr
+
     if((name_length != 0) && ((name_length + 1 /* first length byte */)  == rr_name_length) )
     {
         rr_name++; /* Skip first length byte */
@@ -616,7 +620,7 @@ static fnet_bool_t _fnet_dns_cmp_name(const char *rr_name, const char *name)
     }
 
     if(i == name_length)
-    {
+    {   Serial.println("Result: FNET_TRUE");
         result = FNET_TRUE;
     }
     else
